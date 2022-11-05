f90f29a17c66f28a0a8b18e4f454571f7bfa87de
Lucas Abrantes Sarmento <lucas_abrantes25@hotmail.com>
2022-11-04 22:11:41 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "Hash.cpp"
+
+int main(){
+    hash H;
+    init(H);
+
+    // porcentTable(H);
+
+    inserirTodos(H, divisao);
+
+    // porcentTable(H);
+
+    remover(H, buscar(H, 110001, divisao), divisao);
+    buscar(H, 120001, divisao);
+
+    // dataItem *d = (dataItem *)malloc(sizeof(dataItem));
+    // d->city.cidade = (char *)"São João do Rio do Peixe";
+    // d->city.id = 250070;
+    // d->city.estado = (char *)"RN";
+    // d->key = 250070;
+    // d->GPS.id = 250070;
+    // d->GPS.la = -6.72;
+    // d->GPS.lo = -38.44;
+
+    // inserir(H, d, divisao);
+
+    return 0;
+}
